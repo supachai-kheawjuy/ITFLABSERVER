291155af258f0210936c480d47d2e15f738e408a
Spicyz <bossthth1209mine@gmail.com>
2020-11-15 02:01:54 +0700

    Update show.php

--- show.php
+++ show.php
@@ -7,59 +7,69 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
-<body>
-  <?php
-  $conn = mysqli_init();
-  mysqli_real_connect($conn, 'itflabserver.mysql.database.azure.com', 'itflab@itflabserver', 'Boss0899046417', 'ITFlab', 3306);
-  if (mysqli_connect_errno($conn)) {
-    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+<script>
+  function Delete(id) {
+    document.getElementById(id).remove();
   }
-  $res = mysqli_query($conn, 'SELECT * FROM guestbook');
-  ?>
-  <table class="table table-hover" width="600" border="1">
-    <thead class="thead-dark">
-      <tr>
-        <th width="100">
-          <div align="center">Name</div>
-        </th>
-        <th width="350">
-          <div align="center">Comment </div>
-        </th>
-        <th width="150">
-          <div align="center">Link </div>
-        </th>
-        <th width="150">
-          <div align="center">Action</div>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
+</script>
 
-    </tbody>
+<body>
+  <div class="card-body">
     <?php
-    while ($Result = mysqli_fetch_array($res)) {
-    ?>
-      <tr>
-        <td>
-          <?php echo $Result['Name']; ?></div>
-        </td>
-        <td>
-          <?php echo $Result['Comment']; ?>
-        </td>
-        <td>
-          <?php echo $Result['Link']; ?>
-        </td>
-        <td>
-          <button type="button" class="btn btn-primary">Edit</button>
-          <button type="button" class="btn btn-danger">Delete</button>
-        </td>
-      </tr>
-    <?php
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, 'itflabserver.mysql.database.azure.com', 'itflab@itflabserver', 'Boss0899046417', 'ITFlab', 3306);
+    if (mysqli_connect_errno($conn)) {
+      die('Failed to connect to MySQL: ' . mysqli_connect_error());
     }
+    $res = mysqli_query($conn, 'SELECT * FROM guestbook');
     ?>
-  </table>
-  <?php mysqli_close($conn); ?>
-  <button type="button" class="btn btn-success">Add Row</button>
+    <table class="table table-hover" width="600" border="1">
+      <thead class="thead-dark">
+        <tr>
+          <th width="100">
+            <div align="center">Name</div>
+          </th>
+          <th width="350">
+            <div align="center">Comment </div>
+          </th>
+          <th width="150">
+            <div align="center">Link </div>
+          </th>
+          <th width="150">
+            <div align="center">Action</div>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        while ($Result = mysqli_fetch_array($res)) {
+        ?>
+          <tr id="# + <?php echo $Result['ID']; ?>">
+            <td>
+              <?php echo $Result['Name']; ?>
+            </td>
+            <td>
+              <?php echo $Result['Comment']; ?>
+            </td>
+            <td>
+              <?php echo $Result['Link']; ?>
+            </td>
+            <td>
+              <button type="button" class="btn btn-primary">Edit</button>
+              <button type="button" class="btn btn-danger" onclick="Delete()">Delete</button>
+            </td>
+          </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+    <?php mysqli_close($conn); ?>
+    <button type="button" class="btn btn-success">Add Row</button>
+  </div>
+  <div class="card mt-3">
+    <p>HELLO WORLD</p>
+  </div>
 </body>
 
 </html>
