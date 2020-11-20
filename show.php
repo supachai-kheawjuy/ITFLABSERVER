028cfa0240268ba863945c37537ce594da9ae62f
Spicyz <bossthth1209mine@gmail.com>
2020-11-20 12:48:41 +0700

    add refresh

--- show.php
+++ show.php
@@ -5,6 +5,7 @@
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <script src="./app.js"></script>
   <style>
     .card {
       margin-top: 2rem;
@@ -18,10 +19,6 @@
 </head>
 
 <script>
-  function Refresh() {
-    document.getElementById("data").Refresh();
-  }
-
   function Delete(id) {
     document.getElementById('row' + id).remove();
   }
@@ -43,7 +40,6 @@
           </center>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
           <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="Delete()">Delete</button>
         </div>
       </div>
