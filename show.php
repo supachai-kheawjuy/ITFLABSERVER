b48c34ca7955c9d2e2e00b0ba1d679cc986fd408
Spicyz <bossthth1209mine@gmail.com>
2020-11-21 15:42:30 +0700

    Update show.php

--- show.php
+++ show.php
@@ -2,7 +2,7 @@
 
 <head>
   <title>ITFLab: Database</title>
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <style>
@@ -35,12 +35,12 @@
       $(this).find('form').trigger('reset');
     })
 
-    $(".deleteeiei").click(function() {
+    $('.deleteeiei').click(function() {
       $('#valID').val($(this).data('value'));
       $('#deleteaccept').modal('show');
     });
 
-    $(".editeiei").click(function() {
+    $('.editeiei').click(function() {
       $.post('show.php', $(this).data('value'), function(data) {
         console.log(data);
       });
